9c3744de1f92923b39f10a6440f2e5dc6c52bdba
Sebastian Andersen <sebastian@sandersen.co>
2021-08-20 07:19:29 +0200

    Updates NavItem

--- src/lib/Navigation/NavItem.js
+++ src/lib/Navigation/NavItem.js
@@ -3,13 +3,20 @@ import defaultTheme from '../defaultTheme';
 import {Link as A} from 'react-router-dom';
 
 const Li = styled.li`
-  padding: 8px 0;
+  padding: 8px;
 `;
 
 const Link = styled(A)`
   text-decoration: none;
   font-family: ${props => props.theme.font};
+  font-size: ${props => props.theme.sizes.sm};
   color: ${props => props.theme.colors.secondary};
+  
+  transition: color 200ms ease-in;
+  
+  ${Li}:hover & {
+    color: ${props => props.theme.colors.tertiary};
+  }
 `;
 
 Link.defaultProps = defaultTheme;
