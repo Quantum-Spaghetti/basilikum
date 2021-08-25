046ce6c12b680cbcac4e8766201ac3f4095e8ba8
Sebastian Andersen <sebastian@sandersen.co>
2021-08-25 10:29:55 +0200

    Updates GlobalStyle.js

--- src/lib/Utils/GlobalStyle.js
+++ src/lib/Utils/GlobalStyle.js
@@ -2,6 +2,12 @@ import {createGlobalStyle} from "styled-components";
 import defaultStyle from '../defaultTheme';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
   body {
     font-size: ${props => props.theme.baseSize};
     font-family: ${props => props.theme.font};
